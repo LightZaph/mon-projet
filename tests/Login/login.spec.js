410c8c496b070d37860553acb4795ae560ca7d90
Akoa As <88491865+LightZaph@users.noreply.github.com>
2023-01-05 15:16:33 +0100

    Add Login Test

---
+++ tests/Login/login.spec.js
@@ -0,0 +1,21 @@
+import {test, expect, chromium } from '@playwright/test';
+
+test ('Login Automatisation Test', (async () => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('http://localhost:3000/');
+  await page.locator('input[name="username"]').click();
+  await page.locator('input[name="username"]').fill('test');
+  await page.locator('input[name="password"]').click();
+  await page.locator('input[name="password"]').fill('1234');
+  await page.getByRole('button', { name: 'Se connecter' }).click();
+  await page.getByRole('link', { name: 'Se déconnecter' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/success');
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+}));
