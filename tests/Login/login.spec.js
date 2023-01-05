369fabdd0de64446fb56cd71fb28f77ef25fe5ee
Akoa As <88491865+LightZaph@users.noreply.github.com>
2023-01-05 15:32:09 +0100

    Update login.spec.js

--- tests/Login/login.spec.js
+++ tests/Login/login.spec.js
@@ -2,7 +2,7 @@ import {test, expect, chromium } from '@playwright/test';
 
 test ('Login Automatisation Test', (async () => {
   const browser = await chromium.launch({
-    headless: false
+    headless: true
   });
   const context = await browser.newContext();
   const page = await context.newPage();
